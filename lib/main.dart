e586886f9e427646a04c20b6a8c96b24285b8f4a
MasoudMaghsodi <magsodi333@gmail.com>
2025-02-11 18:54:58 +0330

    start agin

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:isar/isar.dart';
-import 'package:path_provider/path_provider.dart';
 // ignore: depend_on_referenced_packages
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'view/homeScreen/home_view.dart';
-import 'model/exerciseModel/exercise_model.dart';
-import 'model/muscle_teq_model/muscle_group_model.dart';
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
@@ -39,18 +34,9 @@ class MyApp extends StatelessWidget {
             GlobalWidgetsLocalizations.delegate,
             GlobalCupertinoLocalizations.delegate,
           ],
-          home: const HomeView(),
+          // home: const HomeView(),
         );
       },
     );
   }
 }
-
-// Provider برای اینیشیالایز کردن Isar
-final isarProvider = FutureProvider<Isar>((ref) async {
-  final dir = await getApplicationDocumentsDirectory();
-  return await Isar.open([
-    MuscleGroupSchema,
-    ExerciseSchema,
-  ], directory: dir.path);
-});
